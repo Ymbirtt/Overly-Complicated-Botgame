e4198657d29c7fe180e8ff5a070fb379dc89a051
Ymbirtt <ymbirtt@gmail.com>
2021-03-03 19:18:30 +0000

    Add anacron script

---
+++ do_poll.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd "$(dirname -- "$0")"
+pwd
+
+source venv/bin/activate
+python overly_complicated_botgame.py post_table > last_run.out
+
+if [ $? -ne 0 ]
+then
+    notify-send "Overly Complicated Botgame failed for some reason"
+fi
