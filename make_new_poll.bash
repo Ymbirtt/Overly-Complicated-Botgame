d899ef45037aea96fae199128ada271b8fbc70b5
Ymbirtt <ymbirtt@gmail.com>
2021-09-10 11:17:50 +0100

    Redirect stderr in bash scripts

--- make_new_poll.bash
+++ make_new_poll.bash
@@ -4,8 +4,8 @@ cd "$(dirname -- "$0")"
 pwd
 
 source venv/bin/activate
-echo "Yes please" | python overly_complicated_botgame.py clear_messages > last_run.out
-python overly_complicated_botgame.py post_poll_message >> last_run.out
+echo "Yes please" | python overly_complicated_botgame.py clear_messages 2>&1 > last_run.out
+python overly_complicated_botgame.py post_poll_message 2>&1 >> last_run.out
 
 if [ $? -ne 0 ]
 then
