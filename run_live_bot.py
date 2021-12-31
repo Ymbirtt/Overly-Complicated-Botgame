ef9a847452baac60a1cf267fe63fda51986a89b3
ymbirtt <ymbirtt@gmail.com>
2021-12-31 17:29:47 +0000

    Add support for tagging a specific role

--- run_live_bot.py
+++ run_live_bot.py
@@ -5,6 +5,7 @@ TOKEN = os.environ['OCB_TOKEN']
 GUILD_ID = os.environ['OCB_GUILD_ID']
 CHANNEL_NAME = os.environ['OCB_CHANNEL_NAME']
 DUMP_CHANNEL_NAME = os.environ['OCB_DUMP_CHANNEL_NAME']
+ROLE_ID = os.environ.get('OCB_ROLE_ID', 0)
 
 
 def main():
@@ -12,6 +13,7 @@ def main():
     bot = LiveBot(guild_id=int(GUILD_ID),
                   channel_name=CHANNEL_NAME,
                   dump_channel_name=DUMP_CHANNEL_NAME,
+                  role_id=int(ROLE_ID),
                   command_prefix='!')
     bot.run(TOKEN)
 
